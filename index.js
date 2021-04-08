43ced8a191864315515e222991cf5ad42397b8ea
jalpa Shah <jalpa-shah@lambdastudents.com>
2021-04-08 17:46:34 -0400

    cors added

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const dotenv = require("dotenv").config()
 const express = require("express")
+const cors = require("cors")
 const app = express()
 const port = process.env.PORT || 7000
+
+app.use(cors())
+app.use(express.json())
 
 app.use("/api/", (_, res) =>{
     res.json({data:"API is accounted for"})
