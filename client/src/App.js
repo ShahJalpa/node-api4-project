8c771d4782b094af056e34fa6adb0d3baae6da5b
jalpa Shah <jalpa-shah@lambdastudents.com>
2021-04-08 20:07:45 -0400

    backend and frontend talking with eachother now

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import { useState, useEffect } from 'react'
 
+function url(path){
+  return process.env.NODE_ENV === "development"
+   ?`http://localhost:8000${path}` : path
+}
 function App() {
+  const [data, setData] = useState("Hi")
+
+  useEffect(() => {
+    fetch(url("/api"))
+      .then(res => res.json())
+      .then(apiData => setData(apiData.data))
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
-          Hello There!!        
+           {data}       
       </header>
     </div>
   );
